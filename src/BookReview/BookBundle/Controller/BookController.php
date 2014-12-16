6d57193a52fb699cf3c005a6ebecdacfc0469f44
Monique Adamson <madamson@hotmail.co.uk>
2014-12-16 18:33:19 +0000

    Error in creating book
    Follow tutorial again and have another attempt

--- src/BookReview/BookBundle/Controller/BookController.php
+++ src/BookReview/BookBundle/Controller/BookController.php
@@ -2,10 +2,7 @@
 
 namespace BookReview\BookBundle\Controller;
 
-use BookReview\BookBundle\Entity\Book;
-use BookReview\BookBundle\Form\BookType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class BookController extends Controller
 {
@@ -15,34 +12,10 @@ class BookController extends Controller
                 // ...
             ));    }
 
-    public function createAction(Request $request)
+    public function createAction()
     {
-        //Create a new empty Book entity
-        $book = new Book();
-
-        //Create a form from the BookType class, validate it against the Book entity with fields
-        //set the form action to the current URI/link
-        $form = $this->createForm(new BookType(), $book,[
-            'action' => $request->getUri() ]);
-
-        //If the request is POST then populate the form
-        $form->handleRequest($request);
-
-        //check if the form is valid
-        if($form->isValid()){
-            //gets the doctrine entity manage
-            $em = $this->getDoctrine()->getManager();
-
-            //tells the manager you want to persist this book entry
-            $em->persist($book);
-
-            //commit all changes
-            $em->flush();
-
-        }
-
         return $this->render('BookReviewBookBundle:Book:create.html.twig', array(
-                'form' => $form->createView()
+                // ...
             ));    }
 
     public function editAction()
