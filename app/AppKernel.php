486c203c58533497875f28496af5a154aa95cd6e
Monique Adamson <madamson@hotmail.co.uk>
2014-12-16 16:45:25 +0000

    Stage One in developmemt
    FOSUserbundle installed, imported the routes and created links for them. Able to login or out, register a new user account

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new BookReview\BookBundle\BookReviewBookBundle(),
             new BookReview\UserBundle\BookReviewUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
